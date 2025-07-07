bb8ffc688cc97960ffebdb3e3cd3c417194172de
Paraxanos <pranjal.s24@iiits.in>
2025-07-07 07:50:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+document.querySelector(".button-34").addEventListener('click',function(){
+    this.classList.add("hide");
+    document.querySelector(".scoreboard").classList.remove("hide");
+    document.querySelector(".row1").classList.remove("hide");
+    document.querySelector(".row2").classList.remove("hide");
+    document.querySelector(".row3").classList.remove("hide");
+    document.querySelector(".rules").classList.remove("hide");
+    var lives=3;
+    var score=0;
+    var l=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    var x=setInterval(() => {
+        var target=l[Math.floor(Math.random()*26)];
+        document.querySelector("."+target).innerHTML="🎯";
+        document.addEventListener("keydown",logic);
+        function logic(event){
+            if(event.key==target){
+                score++;
+                document.querySelector(".scoreboard").innerHTML="<h2 style='font-family:Tron'>Score:"+score;
+                document.querySelector("."+target).innerHTML=target.toUpperCase();
+            }
+            else{
+                lives--;
+                if(lives<=0){
+                    document.querySelector(".Title>h1").innerHTML="Game Over!You fought well, Refresh to Restart.";
+                    clearInterval(x);
+                }
+            }
+        };
+        setTimeout(() => {
+            document.removeEventListener("keydown",logic);
+            document.querySelector("."+target).innerHTML=target.toUpperCase();
+        }, 3000);
+        console.log(lives);
+        console.log(target);
+    }, 7000);
+});
